e00d2e14acadc083016ac03b2b354cd94e84d1bd
alexgagnon <alex@monad.media>
2024-03-27 23:48:45 -0400

    updates

--- benches/decompress_and_json.sh
+++ benches/decompress_and_json.sh
@@ -3,8 +3,7 @@
 FILTER='select((.type == "item") and (.labels | has("en")) and (.claims.P31 // [] | map(.mainsnak.datavalue.value.id != "Q13442814") | all)) | (.id|ltrimstr("Q")) as $id | .labels["en"].value\, (.aliases["en"] // [] | map(.value))[] | [.\, $id] | @tsv'
 
 hyperfine --show-output --export-markdown decompress_and_json_results.md -L filter "$FILTER" -L i 1 -L b 1000000000 -r 5 \
-$'./jbzip2-shell -i ../samples/data/{i}.json.bz2 -j \'{filter}\' -p \'[\n\' -s \'\n]\' -d \',\n\' -b {b}' \
-$'bunzip2 --keep -c ../samples/data/{i}.json.bz2 | ../../jstream/jstream -d 1 | jq -r \'{filter}\'' \
-# $'bunzip2 --keep -c ../samples/data/{i}.json.bz2 | jq -r --stream \'fromstream(1|truncate_stream(inputs)) | {filter}\'' \
-# $'lbunzip2 --keep -c ../samples/data/{i}.json.lbz2 | jq -r --stream \'fromstream(1|truncate_stream(inputs)) | {filter}\'' \
+$'../../jbzip2-shell -i ../samples/data/{i}.json.bz2 -j \'{filter}\' -p \'[\n\' -s \'\n]\' -d \',\n\' -b {b}' \
+$'../../jbzip2-linked -i ../samples/data/{i}.json.bz2 -j \'{filter}\' -p \'[\n\' -s \'\n]\' -d \',\n\' -b {b}' \
+# $'bunzip2 --keep -c ../samples/data/{i}.json.bz2 | ../../jstream/jstream -d 1 | jq -r \'{filter}\'' \
 # $'lbunzip2 --keep -c ../samples/data/{i}.json.lbz2 | jstream -d 1 | jq -r \'{filter}\'' \
