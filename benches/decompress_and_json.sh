30caec9e7ccd1aa6dab685f523c5b7b7f773ba0a
alexgagnon <alex@monad.media>
2024-03-30 09:53:21 -0400

    updated actions

--- benches/decompress_and_json.sh
+++ benches/decompress_and_json.sh
@@ -2,7 +2,7 @@
 
 FILTER='select((.type == "item") and (.labels | has("en")) and (.claims.P31 // [] | map(.mainsnak.datavalue.value.id != "Q13442814") | all)) | (.id|ltrimstr("Q")) as $id | .labels["en"].value\, (.aliases["en"] // [] | map(.value))[] | [.\, $id] | @tsv'
 
-for i in 1 10 100 1_000 10_000 100_000 1_000_000; do
+for i in 10_000 100_000; do
   hyperfine --show-output -i -u second -L filter "$FILTER" -L i $i -r 5 \
   --export-markdown decompress_and_json_results_$i.md \
   --command-name jbzip2 \
