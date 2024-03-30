bac12e2c1edcecb3c278367328b19563371883c1
alexgagnon <alex@monad.media>
2024-03-30 15:41:42 -0400

    update to version pull

--- benches/json_only.sh
+++ benches/json_only.sh
@@ -2,9 +2,8 @@
 
 FILTER='select((.type == "item") and (.labels | has("en")) and (.claims.P31 // [] | map(.mainsnak.datavalue.value.id != "Q13442814") | all)) | (.id|ltrimstr("Q")) as $id | .labels["en"].value\, (.aliases["en"] // [] | map(.value))[] | [.\, $id] | @tsv'
 
-for i in 1_000_000; do
+for i in 1, 10, 100, 1_000, 10_000, 100_000, 1_000_000; do
   hyperfine --show-output -i -u second -L filter "$FILTER" -L i $i -r 5 \
-  --export-markdown json_only_results_24gb_$i.md \
   --command-name jq \
   --command-name 'jq-stream' \
   --command-name jm \
