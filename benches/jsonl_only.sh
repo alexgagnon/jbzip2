50dd5aa1f2dd05707dc2dcc27531557b140b6c4e
alexgagnon <alex@monad.media>
2024-03-30 22:09:55 -0400

    more benches

---
+++ benches/jsonl_only.sh
@@ -0,0 +1,10 @@
+# Benchmarks - Processing only
+
+FILTER='select((.type == "item") and (.labels | has("en")) and (.claims.P31 // [] | map(.mainsnak.datavalue.value.id != "Q13442814") | all)) | (.id|ltrimstr("Q")) as $id | .labels["en"].value\, (.aliases["en"] // [] | map(.value))[] | [.\, $id] | @tsv'
+
+for i in 1 10 100 1_000 10_000 100_000 1_000_000; do
+  hyperfine --show-output -i -u second -L filter "$FILTER" -L i $i -r 5 \
+  --command-name jq \
+  --export-markdown jsonl_only_results_8gb_$i.md \
+  $'jq -r \'{filter}\' < ../samples/data/{i}.jsonl'
+done
