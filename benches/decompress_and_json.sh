83e0a0a2610c661c52a90ab5af58d75f52737dc4
alexgagnon <alex@monad.media>
2024-04-01 11:51:11 -0400

    add some examples

--- benches/decompress_and_json.sh
+++ benches/decompress_and_json.sh
@@ -2,12 +2,13 @@
 
 FILTER='select((.type == "item") and (.labels | has("en")) and (.claims.P31 // [] | map(.mainsnak.datavalue.value.id != "Q13442814") | all)) | (.id|ltrimstr("Q")) as $id | .labels["en"].value\, (.aliases["en"] // [] | map(.value))[] | [.\, $id] | @tsv'
 
-for i in 10_000 100_000; do
+for i in 1 10 100 1_000 10_000 100_000; do
   hyperfine --show-output -i -u second -L filter "$FILTER" -L i $i -r 5 \
   --export-markdown decompress_and_json_results_$i.md \
   --command-name jbzip2 \
   --command-name jstream \
   -L b 1000000000 \
-  $'../target/release/jbzip2 -i ../samples/data/{i}.json.bz2 -j \'{filter}\' -p \'[\n\' -s \'\n]\' -d \',\n\' -b {b}' \
-  $'bunzip2 --keep -c ../samples/data/{i}.json.bz2 | ../../jstream/jstream -d 1 | jq -r \'{filter}\''
+  $'../target/release/jbzip2 -i ../samples/data/{i}.json.bz2 -j \'{filter}\' -t wikidump \
+  $'bunzip2 --keep -c ../samples/data/{i}.json.bz2 | ../../jstream/jstream -d 1 | jq -r \'{filter}\'' \
+  $'lbunzip2 --keep -c ../samples/data/{i}.json.bz2 | ../../jstream/jstream -d 1 | jq -r \'{filter}\'' \
 done
